d23d98327abdc93de632b2b8d5004a6f405759fd
djumanov <diyorbekjumanov1120@gmail.com>
2023-02-15 18:10:28 +0500

    bot

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from flask import Flask, request
+from telegram import Bot
+import os
+
 
 echo_app = Flask(__name__)
+
+TOKEN = os.environ['TOKEN']
+bot = Bot(TOKEN)
+
 
 @echo_app.route('/', methods=['POST', 'GET'])
 def main():
@@ -8,23 +15,13 @@ def main():
         return 'hi from Husniddin'
     elif request.method == 'POST':
         data = request.get_json(force=True)
-        # data = {
-        #     'update_id': 366164406, 
-        #     'message': {
-        #         'message_id': 151, 
-        #         'from': {'id': 1258594598, 'is_bot': False, 'first_name': 'Diyorbek', 'last_name': 'Jumanov', 'username': 'jumanovdiyorbek', 'language_code': 'en'}, 
-        #         'chat': {'id': 1258594598, 'first_name': 'Diyorbek', 'last_name': 'Jumanov', 'username': 'jumanovdiyorbek', 'type': 'private'}, 
-        #         'date': 1676463994, 
-        #         'text': '/start', 
-        #         'entities': [{'offset': 0, 'length': 6, 'type': 'bot_command'}
-        #         ]
-        #     }
-        # }
 
         chat_id = data['message']['from']['id']
         text = data['message']['text']
 
         print(chat_id, text)
+        bot.send_message(chat_id, text)
+        
         return 'hello'
 
 if __name__ == '__main__':
