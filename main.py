737e8219fbe77c0249c4baa472d4499d645dbed2
djumanov <diyorbekjumanov1120@gmail.com>
2023-02-15 17:32:34 +0500

    init

--- main.py
+++ main.py
@@ -8,8 +8,23 @@ def main():
         return 'hi from Husniddin'
     elif request.method == 'POST':
         data = request.get_json(force=True)
-        print(data)
+        # data = {
+        #     'update_id': 366164406, 
+        #     'message': {
+        #         'message_id': 151, 
+        #         'from': {'id': 1258594598, 'is_bot': False, 'first_name': 'Diyorbek', 'last_name': 'Jumanov', 'username': 'jumanovdiyorbek', 'language_code': 'en'}, 
+        #         'chat': {'id': 1258594598, 'first_name': 'Diyorbek', 'last_name': 'Jumanov', 'username': 'jumanovdiyorbek', 'type': 'private'}, 
+        #         'date': 1676463994, 
+        #         'text': '/start', 
+        #         'entities': [{'offset': 0, 'length': 6, 'type': 'bot_command'}
+        #         ]
+        #     }
+        # }
 
+        chat_id = data['message']['from']['id']
+        text = data['message']['text']
+
+        print(chat_id, text)
         return 'hello'
 
 if __name__ == '__main__':
