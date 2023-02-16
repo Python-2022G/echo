5ecde94e03cb800990b4043f613301bccc1039ac
djumanov <diyorbekjumanov1120@gmail.com>
2023-02-16 09:39:44 +0500

    init

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from flask import Flask, request
-from telegram import Bot, Update
+# from telegram import Bot, Update
 import os
 
 
 echo_app = Flask(__name__)
 
-TOKEN = os.environ['TOKEN']
-bot = Bot(TOKEN)
+# TOKEN = os.environ['TOKEN']
+# bot = Bot(TOKEN)
 
 
 @echo_app.route('/', methods=['POST', 'GET'])
@@ -16,7 +16,20 @@ def main():
 
     elif request.method == 'POST':
         data = request.get_json(force=True) # get data from request
+        
+        # {
+        #     'update_id': 366164447, 
+        #     'message': {
+        #         'message_id': 225, 
+        #         'from': {'id': 1258594598, 'is_bot': False, 'first_name': 'Diyorbek', 'last_name': 'Jumanov', 'username': 'jumanovdiyorbek', 'language_code': 'en'}, 
+        #         'chat': {'id': 1258594598, 'first_name': 'Diyorbek', 'last_name': 'Jumanov', 'username': 'jumanovdiyorbek', 'type': 'private'}, 
+        #         'date': 1676521987, 
+        #         'text': 'salom'
+        #         }
+        # }
+        chat_id = data['message']['chat']['id']
+        text = data['message']['text']
 
-        print(data)
+        print(chat_id, text)
 
         return 'hello'
